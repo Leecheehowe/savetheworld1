a8f1273236c8e3065261f83a205f884251ecf6e8
chee howe howe <125532800+Leecheehowe@users.noreply.github.com>
2023-03-18 08:33:51 +0000

    everything

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def save_suggestion():
+    if 'dontsubmit' in request.form:
+        return render_template('love.html')
+    remarks = request.form['remarks']
+    with open('remarks.txt', 'a') as f:
+        f.write(remarks + '\n')
+    return redirect(url_for('thank_you'))
+
+@app.route('/thankyou')
+def thank_you():
+    return render_template('thankyou.html')
+
+@app.route('/love')
+def love():
+    return render_template('love.html')
 
 app.run(host='0.0.0.0', port=81)
